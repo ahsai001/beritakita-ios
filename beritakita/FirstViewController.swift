0d28bdc766280539644addf6a530ba07196965e7
Ahmad Saifullah <ahsai001@gmail.com>
2020-09-04 09:33:06 +0700

    initial commit

---
+++ beritakita/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  beritakita
+//
+//  Created by Ahmad on 04/09/20.
+//  Copyright © 2020 AhsaiLabs. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ beritakita/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  beritakita
+//
+//  Created by Ahmad on 04/09/20.
+//  Copyright © 2020 AhsaiLabs. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
